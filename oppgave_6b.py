68f0b2b4f4d9706a826784434e2e5e96a2caf8e8
victor-sand <victsan@stud.ntnu.no>
2026-02-28 17:24:28 +0100

    gjorde skalaen konstant

--- oppgave_6b.py
+++ oppgave_6b.py
@@ -5,23 +5,26 @@ nodes = 100
 lx = 0.25
 ly = 0.25
 alpha = 9.3e-6
+start_temp = 15
+oven_temp = 200
+minutes = 20
 
 dx = lx / nodes
 dy = ly / nodes
 
 dt = 0.9 / (2 * alpha * (1 / dx ** 2 + 1 / dy ** 2))
-T = 20 * 60
+T = minutes * 60
 nt = int(T / dt)
 
 x = np.linspace(0, lx, nodes)
 y = np.linspace(0, ly, nodes)
 
-u = np.ones((nodes, nodes)) * 15
+u = np.ones((nodes, nodes)) * start_temp
 
-u[0, :] = 200
-u[-1, :] = 200
-u[:, 0] = 200
-u[:, -1] = 200
+u[0, :] = oven_temp
+u[-1, :] = oven_temp
+u[:, 0] = oven_temp
+u[:, -1] = oven_temp
 
 for _ in range(nt):
     u_new = u.copy()
@@ -37,10 +40,11 @@ for _ in range(nt):
 X, Y = np.meshgrid(x, y)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(X, Y, u, cmap='viridis')
+surf = ax.plot_surface(X, Y, u, cmap='viridis', vmin=start_temp, vmax=oven_temp)
 fig.colorbar(surf)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
+ax.set_zlim(start_temp, oven_temp)
 ax.set_zlabel('Temperatur')
 
 plt.show()
