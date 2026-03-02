5f650d17c1d914292ef8942a31b9413a64e5f0df
victor-sand <victsan@stud.ntnu.no>
2026-03-02 21:36:11 +0100

    oppg 7a

---
+++ oppgave_7a.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+nodes = 100
+air = 0.125   # luft på hver side
+lx = 0.25 + 2*air
+ly = 0.25 + 2*air
+alpha_titan = 9.3e-6
+alpha_air = 4.8e-5
+start_temp = 15
+oven_temp = 200
+minutes = 1
+
+dx = lx / nodes
+dy = ly / nodes
+
+plate_start = nodes // 4
+plate_end = 3 * nodes // 4
+
+alpha_field = np.ones((nodes, nodes)) * alpha_air
+alpha_field[
+    plate_start:plate_end,
+    plate_start:plate_end
+] = alpha_titan
+
+alpha_max = max(alpha_titan, alpha_air)
+
+dt = 0.9 / (2 * alpha_max * (1/dx**2 + 1/dy**2))
+T = minutes * 60
+nt = int(T / dt)
+
+x = np.linspace(0, lx, nodes)
+y = np.linspace(0, ly, nodes)
+
+u = np.ones((nodes, nodes)) * oven_temp
+u[
+    plate_start:plate_end,
+    plate_start:plate_end
+] = start_temp
+
+u[0, :] = oven_temp
+u[-1, :] = oven_temp
+u[:, 0] = oven_temp
+u[:, -1] = oven_temp
+
+for _ in range(nt):
+    u_new = u.copy()
+
+    u_new[1:-1, 1:-1] = u[1:-1, 1:-1] + dt * alpha_field[1:-1, 1:-1] * (
+        (u[1:-1, 2:] - 2 * u[1:-1, 1:-1] + u[1:-1, :-2]) / dx ** 2 +
+        (u[2:, 1:-1] - 2 * u[1:-1, 1:-1] + u[:-2, 1:-1]) / dy ** 2
+    )
+    u = u_new
+
+# 3D plot
+X, Y = np.meshgrid(x, y)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.plot_surface(X, Y, u, cmap='viridis', vmin=start_temp, vmax=oven_temp)
+fig.colorbar(surf)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlim(start_temp, oven_temp)
+ax.set_zlabel('Temperatur')
+
+plt.show()
